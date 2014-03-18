f0a0b04713b065137fc23e4e68e512b48b74f95a
Your Name <blake.jennings@gmail.com>
2014-03-18 11:33:20 -0700

    models and controllers basics

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberProducts::Application.config.secret_key_base = '8c36fd98bc4d1afc2f6e6d4d26fbed439d7f146742e4752aa35b1d52b7999d9beea5192de594ef99ffa29dc7cdd6f311246b650982b4b61c4b806a4281d0a41b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmberProducts::Application.config.session_store :cookie_store, key: '_ember_products_session'
