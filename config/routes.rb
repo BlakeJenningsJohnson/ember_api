e8ff08b23bab847d1cbc51e9d6ed90b404d561be
Your Name <blake.jennings@gmail.com>
2014-03-18 12:03:08 -0700

    added cc to order model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ EmberProducts::Application.routes.draw do
 
 defaults format: :json do
   get "/products", to: "products#index"
+  get '/carts', to: 'carts#index'
+  get '/items', to: 'items#index'
+  get '/orders', to: 'orders#index'
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ db/migrate/20140318185349_add_cc_to_order.rb
@@ -0,0 +1,5 @@
+class AddCcToOrder < ActiveRecord::Migration
+  def change
+    add_column :orders, :credit_card_number, :string
+  end
+end
